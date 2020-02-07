d5697ce46cf30598d0e43266ec6f4a081849b13c
Tiago Martins <vargas.tiago@hotmail.com>
2020-02-06 23:54:50 -0300

    Finalized the script to get the products and added animations

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,36 +1,51 @@
-function getApi(url) {
-    axios.get(url)
-        .then(response => {
-          
+document.addEventListener("DOMContentLoaded", () => {
+    let urlApi = "frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1";
+    getProducts(urlApi);
+});
 
+document.getElementById("button-load-more").onclick = () => {
+    startPreloader();
+    getProducts(urlApi);
+}
+
+function getProducts(url) {
+    axios.get(`https://${url}`)
+        .then(response => {
+                showProducts(response.data.products)
+                urlApi = response.data.nextPage;
         })
         .catch(error => {
-            console.log(error)
-        });
+            console.log(error.message)
+        })
+        .finally(() => endPreloader());
 }
-
 
 function showProducts(data){
-    data.forEach(element => {
-        document.getElementById("products").innerHTML = 
-        `<article class="product">
-        <figure style="background-image: url('http://imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=5783178');"></figure>
-        <h3>${element}</h3>
-        <p>Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.</p>
-        <span class="old-price">De: R$23,99</span>
-        <span class="price">Por: R$19,99</span>
-        <span class="installment">ou 2x de R$9,99</span>
-        <button>Comprar</button>
-        </article>`;
+    data.forEach(product => {
+        const article = document.createElement('article');
+        article.className = 'product animated fadeIn';
+        article.innerHTML = `<figure style="background-image: url('${product.image}');"></figure>
+            <h3>${product.name}</h3>
+            <p>${product.description}</p>
+            <span class="old-price">De: R$${organizePrice(product.oldPrice)}</span>
+            <span class="price">Por: R$${organizePrice(product.price)}</span>
+            <span class="installment">ou ${organizePrice(product.installments.count)}x de R$${organizePrice(product.installments.value)}</span>
+            <button>Comprar</button>`;
+        
+        document.getElementById("products").appendChild(article);
     });
-    
 }
 
-function nextPage(url){
-   try {
-    const response = getData(url);
-    return response.data.nextPage;
-   } catch (error) {
-       console.log(error)
-   }
+function organizePrice(price){
+    return price.toFixed(2).toString().replace(".", ",");
+}
+
+function startPreloader() {
+    document.getElementById('preloader').style.display = 'block'
+    document.getElementById('button-load-more').style.display = 'none'
+}
+
+function endPreloader() {
+    document.getElementById('preloader').style.display = 'none'
+    document.getElementById('button-load-more').style.display = 'block'
 }
