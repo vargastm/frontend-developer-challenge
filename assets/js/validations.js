9fb817c743647b3a20b4574f32755aff6698e95c
Tiago Martins <vargas.tiago@hotmail.com>
2020-02-10 15:43:30 -0300

    Refactored function

--- assets/js/validations.js
+++ assets/js/validations.js
@@ -1,45 +1,38 @@
-var form = document.getElementById("form-contact");
+const form = document.getElementById("form-contact");
  
 if (form.addEventListener) {                   
     form.addEventListener("submit", validateRegistration);
 } else if (form.attachEvent) {                  
     form.attachEvent("onsubmit", validateRegistration);
 }
- 
-function validateRegistration(evt) {
-	var name = document.getElementById('name');
-    var email = document.getElementById('email');
-    var filterName = /^[A-zÀ-ÿ][A-zÀ-ÿ']+\s([A-zÀ-ÿ']\s?)*[A-zÀ-ÿ][A-zÀ-ÿ']+$/
-	var filterEmail = /^[a-zA-Z0-9][a-zA-Z0-9\._-]+@([a-zA-Z0-9\._-]+\.)[a-zA-Z-0-9]{2,3}/
-	var countError = 0;
 
-	nameBox = document.querySelector('.message-name');
-	if(name.value == "") {
-		nameBox.innerHTML = "Favor preencher o Nome";
-		nameBox.style.display = 'block';
-		countError += 1;
-	}else if(filterName.test(name.value)) {
-		nameBox.style.display = 'none';
-	} else {
-        nameBox.innerHTML = "Favor preencher o Nome e Sobrenome corretamente";
-        nameBox.style.display = 'block';
-        countError += 1;
-    }
- 
-	emailBox = document.querySelector('.message-email');
-	if(email.value == ""){
-		emailBox.innerHTML = "Favor preencher o E-mail";
-		emailBox.style.display = 'block';
-		countError += 1;
-	}else if(filterEmail.test(email.value)) {
-		emailBox.style.display = 'none';
-	} else {
-		emailBox.innerHTML = "Formato do E-mail inválido";
-		emailBox.style.display = 'block';
-		countError += 1;
-	}	    
- 
-	if(countError > 0){
-		evt.preventDefault();
+function validateRegistration(event) {
+	const name = document.getElementById('name');
+	const email = document.getElementById('email');
+	const nameBox = document.querySelector('.message-name');
+	const emailBox = document.querySelector('.message-email');
+    const filterName = /^[A-zÀ-ÿ][A-zÀ-ÿ']+\s([A-zÀ-ÿ']\s?)*[A-zÀ-ÿ][A-zÀ-ÿ']+$/;
+	const filterEmail = /^[a-zA-Z0-9][a-zA-Z0-9\._-]+@([a-zA-Z0-9\._-]+\.)[a-zA-Z-0-9]{2,3}/;
+	
+	const validateName = validateInput(name, nameBox, filterName);
+	const validateEmail = validateInput(email, emailBox, filterEmail);
+
+	if(!validateName || !validateEmail) {
+		event.preventDefault();
 	}
+}
+
+function validateInput(input, box, filter) {
+	if (!input.value) {
+		box.innerHTML = "Este campo não pode ser vazio!";
+		box.style.display = 'block';
+		return false;
+	}
+	if (filter.test(input.value)) {
+		box.style.display = 'none';
+		return true;
+	}
+	box.innerHTML = "Favor preencher o campo corretamente";
+	box.style.display = 'block';
+	return false;
 }
